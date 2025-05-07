8cceb07fd7da39423d274a5deefe22e829d725a8
mr-abdellah <belkaid.abdulah@gmail.com>
2025-05-07 17:30:33 +0100

    fix(api): update base URL to use the production API endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { getToken } from "@/utils/token";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://ged-back.mr-abdellah.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
